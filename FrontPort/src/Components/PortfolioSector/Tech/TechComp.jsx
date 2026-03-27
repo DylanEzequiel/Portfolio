aa0a760f85e10818e3d83395b15ba0eafbc93ada
dylan correa <D.correa.E05@gmail.com>
2026-03-27 15:17:24 -0300

    chore: skills updated

--- FrontPort/src/Components/PortfolioSector/Tech/TechComp.jsx
+++ FrontPort/src/Components/PortfolioSector/Tech/TechComp.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { FaDatabase } from 'react-icons/fa'
+import { MdDeviceHub, MdOutlineStyle } from 'react-icons/md'
+import { LuAppWindow,LuBrainCircuit } from "react-icons/lu";
+function TechComp({title,iType,color}) {
+  function ityreRender(iType,color){
+    const style = { color };
 
-function TechComp({title,image}) {
+  if (iType === "STY") return <MdOutlineStyle style={style} size={35} />
+  if (iType === "FE") return <LuAppWindow style={style} size={35}/>
+  if (iType === "DEV") return <MdDeviceHub style={style} size={35}/>
+  if (iType === "BE") return <LuBrainCircuit style={style} size={35}/>
+  if (iType === "DB") return <FaDatabase style={style} size={35}/>
+
+  }
   return (
-    <div className='font-semibold text-[#bbb] hover:text-white text-sm md:text-base text-center hover:-translate-y-1 duration-200'>
-                <img src={image} alt={`${title} Icon`} className='m-auto h-14' /> 
-                <h5>{title}</h5>
-              </div>
+    <div className='flex items-center bg-gray-50/20 rounded-md w-[10rem] h-[3rem] font-semibold text-white text-sm text-center hover:-translate-y-1 duration-200'>
+      <div className='p-2'>
+                {ityreRender(iType,color)}
+      </div>
+                <h5 className='self-center m-auto'>{title}</h5>
+    </div>
   )
 }
 
---
+++ FrontPort/src/views/PortfolioViews/Skills/skillsData.js
@@ -0,0 +1,55 @@
+const skills = [
+  {
+    title: "Frontend",
+    items: [
+      { title: "HTML", type: "STY", color: "#3B82F6" },
+      { title: "CSS", type: "STY", color: "#10B981" },
+      { title: "React", type: "FE", color: "#F59E0B" },
+      { title: "Next.js", type: "FE", color: "#EF4444" },
+      { title: "TailwindCSS", type: "STY", color: "#3B82F6" },
+      { title: "Bootstrap", type: "STY", color: "#10B981" },
+      { title: "Figma", type: "STY", color: "#F59E0B" },
+      { title: "Angular", type: "FE", color: "#EF4444" },
+      { title: "React Native", type: "FE", color: "#3B82F6" }
+    ]
+  },
+
+  {
+    title: "Backend",
+    items: [
+      { title: "Express", type: "BE", color: "#3B82F6" },
+      { title: "NestJS", type: "BE", color: "#10B981" },
+      { title: "MongoDB", type: "DB", color: "#3B82F6" },
+      { title: "Node.js", type: "BE", color: "#F59E0B" },
+      { title: "TypeORM", type: "BE", color: "#EF4444" },
+      { title: "MySQL", type: "DB", color: "#10B981" },
+      { title: "REST API", type: "BE", color: "#3B82F6" },
+      { title: "PostgreSQL", type: "DB", color: "#F59E0B" },
+      { title: "GraphQL", type: "BE", color: "#10B981" }
+    ]
+  },
+
+  {
+    title: "Automation & DevOps",
+    items: [
+      { title: "Docker", type: "DEV", color: "#3B82F6" },
+      { title: "CI/CD", type: "DEV", color: "#10B981" },
+      { title: "Git Flow", type: "DEV", color: "#F59E0B" },
+      { title: "Jest", type: "DEV", color: "#EF4444" },
+    ]
+  },
+
+  {
+    title: "Integrations & State Management",
+    items: [
+      { title: "Mercado Pago API", type: "DEV", color: "#3B82F6" },
+      { title: "Redux Toolkit", type: "FE", color: "#3B82F6" },
+      { title: "Stripe", type: "DEV", color: "#10B981" },
+      { title: "Transbank SDK", type: "DEV", color: "#F59E0B" },
+      { title: "Context API", type: "FE", color: "#10B981" },
+      { title: "WhatsApp API", type: "DEV", color: "#EF4444" }
+    ]
+  }
+];
+
+export default skills;
